2e07c433ded210198845ba11c16601d1ddcb7b0c
Ludwing Cano <ludwing238@gmail.com>
2016-05-01 20:38:17 -0600

    SO.sql
    Script with schema, table & user creation.

---
+++ Proyecto Final/SO.sql
@@ -0,0 +1,12 @@
+create user 'pfinal' identified by 'pfinal';
+create schema lista_transaccion;
+grant all privileges on lista_transaccion.* to 'pfinal';
+
+create table lista_transaccion (
+	
+    id integer AUTO_INCREMENT primary key,
+    origen  varchar(5) not null,
+    destino varchar(5)  not null
+    
+
+);
