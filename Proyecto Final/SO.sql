6e63c5048b9b7960087f57c8bb9a9b25e1224d7b
Ludwing Cano <ludwing238@gmail.com>
2016-05-01 20:42:44 -0600

    SO.sql

--- Proyecto Final/SO.sql
+++ Proyecto Final/SO.sql
@@ -1,7 +1,7 @@
 create user 'pfinal' identified by 'pfinal';
 create schema lista_transaccion;
 grant all privileges on lista_transaccion.* to 'pfinal';
-
+use lista_transaccion;
 create table lista_transaccion (
 	
     id integer AUTO_INCREMENT primary key,
